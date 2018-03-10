453a5eac20fa1e9b4b108c24ddc2f91bdaa022b1
C. Annable <christopher.annable@gmail.com>
2018-03-10 00:09:35 -0500

    Update schema.sql to reflect changes for v1.0.

--- schema.sql
+++ schema.sql
@@ -1,27 +1,27 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS 'global' (
-	'setting'	TEXT UNIQUE,
-	'value'	    TEXT,
-	PRIMARY KEY('setting')
+CREATE TABLE 'global' (
+        'setting'       TEXT UNIQUE,
+        'value'     TEXT,
+        PRIMARY KEY('setting')
 );
-CREATE TABLE IF NOT EXISTS 'defaults' (
-	'default'	TEXT UNIQUE,
-	'value'	    TEXT,
-	PRIMARY KEY('default')
+CREATE TABLE 'defaults' (
+    'setting'   TEXT UNIQUE,
+    'value'         TEXT,
+    PRIMARY KEY('setting')
 );
-CREATE TABLE IF NOT EXISTS 'connections' (
-	'index'	        INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	'nickname'      TEXT NOT NULL UNIQUE,
-	'host'	        TEXT NOT NULL,
-	'user'	        TEXT NOT NULL,
-	'description'  	TEXT,
-	'arguments' 	TEXT,
-	'identity'	    TEXT,
-	'command'	    TEXT
+CREATE TABLE 'connections' (
+    'id'         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    'nickname'      TEXT NOT NULL UNIQUE,
+    'host'          TEXT NOT NULL,
+    'user'          TEXT,
+    'description'   TEXT,
+    'args'          TEXT,
+    'identity'      TEXT,
+    'command'       TEXT
 );
 INSERT INTO 'global' (setting,value) VALUES ('schema_version','1.0');
-INSERT INTO 'defaults' (default,value) VALUES ('user',NULL);
-INSERT INTO 'defaults' (default,value) VALUES ('args',NULL);
-INSERT INTO 'defaults' (default,value) VALUES ('identity',NULL);
-INSERT INTO 'defaults' (default,value) VALUES ('command',NULL);
+INSERT INTO 'defaults' (setting,value) VALUES ('user',NULL);
+INSERT INTO 'defaults' (setting,value) VALUES ('args',NULL);
+INSERT INTO 'defaults' (setting,value) VALUES ('identity',NULL);
+INSERT INTO 'defaults' (setting,value) VALUES ('command',NULL);
 COMMIT;
